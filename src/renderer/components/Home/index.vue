feaeeba3fca9eda8cae7c629477fe43b18518163
helpceo <helpceo@126.com>
2018-11-30 17:06:19 +0800

    模板

---
+++ src/renderer/components/Home/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <el-button plain>朴素按钮</el-button>
+    <el-button type="primary" plain>主要按钮</el-button>
+    <el-button type="success" plain>成功按钮</el-button>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'landing-page',
+  };
+</script>
+<style scoped lang="scss" src="./index.scss"></style>
